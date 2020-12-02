e0b755b6ae786f36a912d0ace0c522fe2c942b2b
ilicbojan <bojan.ilic2404@gmail.com>
2020-12-02 22:48:27 +0100

    Finish NUnit Configuration in Application.IntegrationTests Project

--- tests/Application.IntegrationTests/Testing.cs
+++ tests/Application.IntegrationTests/Testing.cs
@@ -1,16 +1,20 @@
 ﻿using API;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using Infrastructure.Identity;
 using Infrastructure.Persistence;
 using MediatR;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using NUnit.Framework;
+using Respawn;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Application.IntegrationTests
@@ -20,6 +24,8 @@ namespace Application.IntegrationTests
     {
         private static IConfiguration _configuration;
         private static IServiceScopeFactory _scopeFactory;
+        private static Checkpoint _checkpoint;
+        private static string _currentUserId;
 
         [OneTimeSetUp]
         public void RunBeforeAnyTests()
@@ -44,7 +50,23 @@ namespace Application.IntegrationTests
 
             startup.ConfigureServices(services);
 
+            // Replace service registration for ICurrentUserService
+            // Remove existing registration
+            var currentUserServiceDescriptor = services.FirstOrDefault(d =>
+                d.ServiceType == typeof(ICurrentUserService));
+
+            services.Remove(currentUserServiceDescriptor);
+
+            // Register testing version
+            services.AddTransient(provider =>
+                Mock.Of<ICurrentUserService>(s => s.UserId == _currentUserId));
+
             _scopeFactory = services.BuildServiceProvider().GetService<IServiceScopeFactory>();
+
+            _checkpoint = new Checkpoint
+            {
+                TablesToIgnore = new[] { "__EFMigrationsHistory" }
+            };
 
             EnsureDatabase();
         }
@@ -56,6 +78,25 @@ namespace Application.IntegrationTests
             var context = scope.ServiceProvider.GetService<AppDbContext>();
 
             context.Database.Migrate();
+        }
+
+        public static async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var mediator = scope.ServiceProvider.GetService<IMediator>();
+
+            return await mediator.Send(request);
+        }
+
+        public static async Task<TEntity> FindAsync<TEntity>(params object[] keyValues)
+            where TEntity : class
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var context = scope.ServiceProvider.GetService<AppDbContext>();
+
+            return await context.FindAsync<TEntity>(keyValues);
         }
 
         public static async Task AddAsync<TEntity>(TEntity entity) 
@@ -70,13 +111,43 @@ namespace Application.IntegrationTests
             await context.SaveChangesAsync();
         }
 
-        public static async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+        public static async Task<string> RunAsDefaultUserAsync()
+        {
+            return await RunAsUserAsync("test@local", "Testing1234!");
+        }
+
+        public static async Task<string> RunAsUserAsync(string email, string password)
         {
             using var scope = _scopeFactory.CreateScope();
 
-            var mediator = scope.ServiceProvider.GetService<IMediator>();
+            var userManager = scope.ServiceProvider.GetService<UserManager<AppUser>>();
 
-            return await mediator.Send(request);
+            var user = new AppUser { UserName = email, Email = email };
+
+            var result = await userManager.CreateAsync(user, password);
+
+            if (result.Succeeded)
+            {
+                _currentUserId = user.Id;
+
+                return _currentUserId;
+            }
+
+            var errors = string.Join(Environment.NewLine, result.ToApplicationResult().Errors);
+
+            throw new Exception($"Unable to create {email}.{Environment.NewLine}{errors}");
+        }
+
+        public static async Task ResetState()
+        {
+            await _checkpoint.Reset(_configuration.GetConnectionString("DefaultConnection"));
+
+            _currentUserId = null;
+        }
+
+        [OneTimeTearDown]
+        public void RunAfterAnyTests()
+        {
         }
     }
 }
