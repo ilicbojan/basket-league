204828bbe29a42f8beaae2895bb3c87a4e4f24ef
ilicbojan <bojan.ilic2404@gmail.com>
2020-12-02 22:49:49 +0100

    Add Tests for GetLeagues and CreateLeague Features

---
+++ tests/Application.IntegrationTests/Leagues/Commands/CreateLeagueTests.cs
@@ -0,0 +1,97 @@
+﻿using Application.Cities.Commands.CreateCity;
+using Application.Common.Exceptions;
+using Application.Countries.Commands.CreateCountry;
+using Application.Leagues.Commands.CreateLeague;
+using Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Application.IntegrationTests.Leagues.Commands
+{
+    using static Testing;
+
+    class CreateLeagueTests : TestBase
+    {
+        [Test]
+        public void ShouldRequireMinimumFields()
+        {
+            var command = new CreateLeagueCommand();
+
+            FluentActions.Invoking(() => SendAsync(command))
+                .Should().Throw<ValidationException>();
+        }
+
+        [Test]
+        public async Task ShouldRequireUniqueName()
+        {
+            var countryId = await SendAsync(new CreateCountryCommand
+            {
+                Name = "Srbija"
+            });
+
+            var cityId = await SendAsync(new CreateCityCommand
+            {
+                Name = "Beograd",
+                CountryId = countryId
+            });
+
+            await SendAsync(new CreateLeagueCommand
+            {
+                Name = "Prva liga",
+                CityId = cityId
+            });
+
+            var command = new CreateLeagueCommand
+            {
+                Name = "Prva liga",
+                CityId = cityId
+            };
+
+            FluentActions.Invoking(() => SendAsync(command))
+                .Should().Throw<ValidationException>();
+        }
+
+        [Test]
+        public void ShouldRequireValidCityId()
+        {
+            var command = new CreateLeagueCommand
+            {
+                Name = "Prva liga",
+                CityId = 1
+            };
+
+            FluentActions.Invoking(() => SendAsync(command))
+                .Should().Throw<ValidationException>();
+        }
+
+        [Test]
+        public async Task ShouldCreateLeague()
+        {
+            var countryId = await SendAsync(new CreateCountryCommand
+            {
+                Name = "Srbija"
+            });
+
+            var cityId = await SendAsync(new CreateCityCommand
+            {
+                Name = "Beograd",
+                CountryId = countryId
+            });
+
+            var command = new CreateLeagueCommand
+            {
+                Name = "Prva liga",
+                CityId = cityId
+            };
+
+            var leagueId = await SendAsync(command);
+
+            var league = await FindAsync<League>(leagueId);
+
+            league.Should().NotBeNull();
+            league.Name.Should().Be(command.Name);
+            league.CityId.Should().Be(command.CityId);
+        }
+    }
+}
--- tests/Application.IntegrationTests/Leagues/Queries/GetLeaguesTests.cs
+++ tests/Application.IntegrationTests/Leagues/Queries/GetLeaguesTests.cs
@@ -1,35 +1,56 @@
-﻿using Application.Leagues.Queries.GetLeagues;
+﻿using Application.Cities.Commands.CreateCity;
+using Application.Countries.Commands.CreateCountry;
+using Application.Leagues.Queries.GetLeagues;
 using Domain.Entities;
 using FluentAssertions;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Application.IntegrationTests.Leagues.Queries
 {
     using static Testing;
 
-    public class GetLeaguesTests
+    public class GetLeaguesTests : TestBase
     {
         [Test]
         public async Task ShouldReturnAllLeagues()
         {
-            // Arrange
+            var countryId = await SendAsync(new CreateCountryCommand
+            {
+                Name = "Srbija"
+            });
+
+            var cityId = await SendAsync(new CreateCityCommand
+            {
+                Name = "Beograd",
+                CountryId = countryId
+            });
+
+            // TODO: Create CreateFieldCommand in Application Project and use SendAsync to get fieldId
+
             await AddAsync(new League
             {
                 Name = "Liga",
-                City = new City { Name = "Beograd", Country = new Country { Name = "Srbija" } }
+                CityId = cityId,
+                Seasons = 
+                {
+                    new Season { Name = "Prolece", Year = 2020, Field = new Field { Name = "Teren 1", Address = "Adresa 1", CityId = cityId } },
+                    new Season { Name = "Leto", Year = 2020, Field = new Field { Name = "Teren 2", Address = "Adresa 2", CityId = cityId } },
+                    new Season { Name = "Jesen", Year = 2020, Field = new Field { Name = "Teren 3", Address = "Adresa 3", CityId = cityId } }
+                }
             });
 
             var query = new GetLeaguesQuery();
 
-            // Act
             var result = await SendAsync(query);
 
-            // Assert
+            result.Should().NotBeNull();
             result.Leagues.Should().HaveCount(1);
+            result.Leagues.First().Name.Should().NotBeNullOrEmpty();
+            result.Leagues.First().City.Should().NotBeNull();
+            result.Leagues.First().Seasons.Should().HaveCount(3);
         }
     }
 }
