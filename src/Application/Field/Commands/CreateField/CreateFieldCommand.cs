cba0d622c23f6dbecd187991ce9ab1828a5f7f59
ilicbojan <bojan.ilic2404@gmail.com>
2020-12-03 22:59:06 +0100

    Add CreateField Feature in API

---
+++ src/Application/Field/Commands/CreateField/CreateFieldCommand.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Application.Common.Interfaces;
+
+namespace Application.Field.Commands.CreateField
+{
+    public class CreateFieldCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int CityId { get; set; }
+    }
+
+    public class CreateFieldCommandHandler : IRequestHandler<CreateFieldCommand, int>
+    {
+        private readonly IAppDbContext _context;
+
+        public CreateFieldCommandHandler(IAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateFieldCommand request, CancellationToken cancellationToken)
+        {
+            var field = new Domain.Entities.Field
+            {
+                Name = request.Name,
+                Address = request.Address,
+                CityId = request.CityId,
+            };
+
+            _context.Fields.Add(field);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return field.Id;
+        }
+    }
+}
---
+++ src/Application/Field/Commands/CreateField/CreateFieldCommandValidator.cs
@@ -0,0 +1,44 @@
+﻿using Application.Common.Interfaces;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Field.Commands.CreateField
+{
+    public class CreateFieldCommandValidator : AbstractValidator<Domain.Entities.Field>
+    {
+        private readonly IAppDbContext _context;
+
+        public CreateFieldCommandValidator(IAppDbContext context)
+        {
+            _context = context;
+
+            RuleFor(x => x.Name)
+              .NotEmpty().WithMessage("Naziv je obavezan")
+              .MaximumLength(50).WithMessage("Naziv ne sme biti duzi od 50 karaktera")
+              .MustAsync(BeUniqueName).WithMessage("Izabrani naziv vec postoji");
+
+            RuleFor(x => x.Address)
+              .NotEmpty().WithMessage("Adresa je obavezna")
+              .MaximumLength(50).WithMessage("Adresa ne sme biti duza od 50 karaktera");
+
+            RuleFor(x => x.CityId)
+              .NotEmpty().WithMessage("Grad je obavezan")
+              .MustAsync(CityExists).WithMessage("Izabrana drzava ne postoji");
+        }
+
+        public async Task<bool> BeUniqueName(string name, CancellationToken cancellationToken)
+        {
+            return await _context.Fields.AllAsync(c => c.Name != name);
+        }
+
+        public async Task<bool> CityExists(int id, CancellationToken cancellationToken)
+        {
+            return await _context.Cities.AnyAsync(c => c.Id == id);
+        }
+    }
+}
