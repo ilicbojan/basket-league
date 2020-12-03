717a687d4a6565bb6f1bb90b951fbe7043a87716
ilicbojan <bojan.ilic2404@gmail.com>
2020-12-03 23:00:59 +0100

    Add Tests for GetTeam and CreateTeam Features

---
+++ tests/Application.IntegrationTests/Teams/Commands/CreateTeamTests.cs
@@ -0,0 +1,59 @@
+﻿using Application.Common.Exceptions;
+using Application.Teams.Commands.CreateTeam;
+using Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IntegrationTests.Teams.Commands
+{
+    using static Testing;
+
+    public class CreateTeamTests : TestBase
+    {
+        [Test]
+        public void ShouldRequireMinimumFields()
+        {
+            var command = new CreateTeamCommand();
+
+            FluentActions.Invoking(() =>
+                SendAsync(command)).Should().Throw<ValidationException>();
+        }
+
+        [Test]
+        public async Task ShouldRequireUniqueName()
+        {
+            await SendAsync(new CreateTeamCommand
+            {
+                Name = "Tim 1"
+            });
+
+            var command = new CreateTeamCommand
+            {
+                Name = "Tim 1"
+            };
+
+            FluentActions.Invoking(() =>
+                SendAsync(command)).Should().Throw<ValidationException>();
+        }
+
+        [Test]
+        public async Task ShouldCreateTeam()
+        {
+            var command = new CreateTeamCommand
+            {
+                Name = "Tim 1"
+            };
+
+            var teamId = await SendAsync(command);
+
+            var team = await FindAsync<Team>(teamId);
+
+            team.Should().NotBeNull();
+            team.Name.Should().Be(command.Name);
+        }
+    }
+}
---
+++ tests/Application.IntegrationTests/Teams/Queries/GetTeamTests.cs
@@ -0,0 +1,30 @@
+﻿using Application.Teams.Commands.CreateTeam;
+using Application.Teams.Queries.GetTeam;
+using Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IntegrationTests.Teams.Queries
+{
+    using static Testing;
+
+    public class GetTeamTests : TestBase
+    {
+        [Test]
+        public async Task ShouldReturnTeam()
+        {
+            var teamId = await SendAsync(new CreateTeamCommand { Name = "Tim 1" });
+
+            var query = new GetTeamQuery { Id = teamId };
+
+            var result = await SendAsync(query);
+
+            result.Should().NotBeNull();
+            result.Name.Should().NotBeNullOrEmpty();
+        }
+    }
+}
