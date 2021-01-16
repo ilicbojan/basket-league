2de22cb6b7ac402c425da0f1007b86e5a0c514a8
ilicbojan <bojan.ilic2404@gmail.com>
2021-01-16 22:21:58 +0100

    Add Style for MatchLineup Component

--- src/web-ui/src/features/matches/lineup/MatchLineup.tsx
+++ src/web-ui/src/features/matches/lineup/MatchLineup.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import React, { useContext } from 'react';
+import Table from '../../../app/common/table/Table';
 import LoadingSpinner from '../../../app/layout/spinner/LoadingSpinner';
 import { RootStoreContext } from '../../../app/stores/rootStore';
 
@@ -7,21 +8,41 @@ const MatchLineup = observer(() => {
   const rootStore = useContext(RootStoreContext);
   const { loadingLineup, lineup } = rootStore.matchPlayerStore;
 
-  if (loadingLineup) return <LoadingSpinner />;
-
   return (
     <div>
-      <div>Lineup and stats</div>
-      {lineup.map((player) => (
-        <div key={player.id}>
-          <div>{player.team.name}</div>
-          <div>{player.firstName}</div>
-          <div>{player.lastName}</div>
-          <div>{player.points}</div>
-          <div>{player.assists}</div>
-          <div>{player.fouls}</div>
-        </div>
-      ))}
+      <Table>
+        <thead>
+          <tr>
+            <th>Player</th>
+            <th>Team</th>
+            <th>PTS</th>
+            <th>AST</th>
+            <th>PF</th>
+          </tr>
+        </thead>
+        <tbody>
+          {loadingLineup ? (
+            <tr>
+              <td className='spin' colSpan={9}>
+                <LoadingSpinner />
+              </td>
+            </tr>
+          ) : (
+            lineup.map((player) => (
+              <tr key={player.id}>
+                <td>
+                  {player.lastName} {player.firstName.slice(0, 1).toUpperCase()}
+                  .
+                </td>
+                <td>{player.team.name.slice(0, 3).toUpperCase()}</td>
+                <td>{player.points}</td>
+                <td>{player.assists}</td>
+                <td>{player.fouls}</td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </Table>
     </div>
   );
 });
